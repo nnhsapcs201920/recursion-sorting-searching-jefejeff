7bc1e35720acf23810cb4ec3579e5e0b4724dab7
jefejeff <54154830+jefejeff@users.noreply.github.com>
2020-03-13 10:29:13 -0500

    intro to recursion

---
+++ ClassNotes/StringReverse.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class StringReverse here.
+ *
+ * @author jcheng3
+ * @version 13 March 2020
+ */
+public class StringReverse
+{
+    public static String reverse(String str)
+    {
+        //must have a terminating case
+        if(str.equals(""))
+        {
+            return str;
+        }
+
+        //must make the problem simple
+        String firstChar = str.substring(0, 1);
+        String restOfString = str.substring(1);
+
+        //recurse - call this method with a simpler problem
+        String restOfStringReversed = reverse(restOfString);
+
+        //build and return the solution
+        String strReversed = restOfStringReversed + firstChar;
+        return strReversed;
+    }
+
+    public static String reverseIter(String str)
+    {
+        String strReversed = "";
+        for(int i = 0; i < str.length(); i++)
+        {
+            strReversed = str.substring(i, i+1) + strReversed;
+        }
+        return strReversed;
+    }
+}
